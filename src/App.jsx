0128d85a7d9b3ea3bc7120a7ca5215f3c15ee80f
JunShen <tony88010955@gmail.com>
2023-10-29 19:51:53 +0800

    'create-all-pages'

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import {
+  HomeLayout,
+  Landing,
+  Error,
+  Products,
+  SingleProduct,
+  Cart,
+  About,
+  Register,
+  Login,
+  Checkout,
+  Orders,
+} from "./pages";
+
 const App = () => {
   return <div className="text-7xl font-bold underline">App</div>;
 };
