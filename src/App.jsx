ac99706f550639a87563497c677f665e8c437162
JunShen <tony88010955@gmail.com>
2023-10-29 20:11:21 +0800

    feat: init route structure

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,54 @@ import {
   Checkout,
   Orders,
 } from "./pages";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "products/:id",
+        element: <SingleProduct />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+      { path: "about", element: <About /> },
+      {
+        path: "checkout",
+        element: <Checkout />,
+      },
+      {
+        path: "orders",
+        element: <Orders />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+    errorElement: <Error />,
+  },
+]);
 
 const App = () => {
-  return <div className="text-7xl font-bold underline">App</div>;
+  return <RouterProvider router={router} />;
 };
-
 export default App;
