83f66b863ac084816dbdc40e1bf6b09fc3d50c74
JunShen <tony88010955@gmail.com>
2023-10-30 17:51:00 +0800

    feat: add Navbar.jsx and NavLinks.jsx

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
-import Header from "../components";
+import { Header, Navbar } from "../components";
 
 const HomeLayout = () => {
   return (
     <>
       <Header />
-      <nav>
-        <span className="text-4xl text-primary">Comfy</span>
-      </nav>
+      <Navbar />
       <section className="align-element py-20">
         <Outlet />
       </section>
